db7dcb40de59f9ee3b64c4bd78e3bcd1594d3734
It's me <r.kobuki@firstconnect.co.jp>
2022-10-27 18:35:00 +0900

    m

--- mieruca-tag.js
+++ mieruca-tag.js
@@ -34,11 +34,13 @@ __fid.push([535598560]);
                   console.log(_.o);
                 },
                 close: ()=> {
+                  console.log(_.o);
                   const _ = window.__mieruca_heatmap.getVariablesFcCustom();
                   _.o.close();
                 },
                 reopen: (url)=>{
                     (()=>{
+                        console.log(_.o);
                         const _ = window.__mieruca_heatmap.getVariablesFcCustom();
                         _.o.close();
                     }
