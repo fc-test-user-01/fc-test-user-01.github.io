cb9eb5fd08b09f08e2f243d9535be8ede0ea9918
root <r.kobuki@firstconnect.co.jp>
2023-11-14 18:31:39 +0900

    m

--- rust-wasm2/test-wasm_bg.wasm.d.ts
+++ rust-wasm2/test-wasm_bg.wasm.d.ts
@@ -3,7 +3,6 @@
 export const memory: WebAssembly.Memory;
 export function set_value(a: number): void;
 export function get_value(): number;
-export function set_string(a: number): number;
 export function get_string(a: number): number;
 export function hello_world(): number;
 export function main(a: number, b: number): number;
