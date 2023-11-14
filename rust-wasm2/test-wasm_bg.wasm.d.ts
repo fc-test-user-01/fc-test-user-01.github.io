4ea3972b98725b0cd782c12a44398de5c63b12c3
root <r.kobuki@firstconnect.co.jp>
2023-11-14 15:25:15 +0900

    m

---
+++ rust-wasm2/test-wasm_bg.wasm.d.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function hello_world(): number;
+export function main(a: number, b: number): number;
+export function __wbindgen_start(): void;
