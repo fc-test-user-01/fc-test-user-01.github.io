c4a8a16c7062e11c97e171236fd3210f1700e659
fc-test-user-01 <r.kobuki+01@firstconnect.co.jp>
2024-03-05 14:28:21 +0900

    m

--- navigate/1/sw.js
+++ navigate/1/sw.js
@@ -1,16 +1,32 @@
-self.registration.update();
+self.addEventListener('install', function(event) {
+  // Perform install steps
+});
+
+self.addEventListener('activate', function(event) {
+  // Activate the new service worker
+  event.waitUntil(self.clients.claim());
+});
+
+// Check for updates to the service worker
+self.addEventListener('message', function(event) {
+  if (event.data && event.data.type === 'checkForUpdate') {
+    self.registration.update();
+  }
+});
+
 self.addEventListener('fetch', event => {
   const url = new URL(event.request.url);
-//  event.respondWith(
-//    new Response('Intercepted content!')
-//  );
   // Inform the client using postMessage API
-  self.clients.matchAll().then(clients => {
-    clients.forEach(client => {
-      client.postMessage({
-        type: 'interceptedRequest',
-        message: 'The request was intercepted!'
+  event.waitUntil(
+    self.clients.matchAll().then(clients => {
+      clients.forEach(client => {
+        client.postMessage({
+          type: 'interceptedRequest',
+          message: 'The request to jobcolo.com was intercepted!'
+        });
       });
-    });
-  });
+    })
+  );
 });
+
+
