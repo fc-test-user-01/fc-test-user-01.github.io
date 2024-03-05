8687767a69013cdc6acaa53fbfc6688b2b2a8709
fc-test-user-01 <r.kobuki+01@firstconnect.co.jp>
2024-03-05 14:18:03 +0900

    m

--- navigate/1/sw.js
+++ navigate/1/sw.js
@@ -1,9 +1,9 @@
 self.registration.update();
 self.addEventListener('fetch', event => {
   const url = new URL(event.request.url);
-  event.respondWith(
-    new Response('Intercepted content!')
-  );
+//  event.respondWith(
+//    new Response('Intercepted content!')
+//  );
   // Inform the client using postMessage API
   self.clients.matchAll().then(clients => {
     clients.forEach(client => {
